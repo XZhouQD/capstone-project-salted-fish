f1abb45e23701c9a765c4ebb18f95dbc691b7edb
unswbei <66148112+unswbei@users.noreply.github.com>
2020-06-22 22:52:52 +1000

    Add files via upload

---
+++ backend/users/smtp.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+
+def send_email(receiver_email,email_content,email_subject):
+    smtp_host = "smtp.exmail.qq.com"
+    smtp_user = "no-reply@mail.x-zhou.com"
+    smtp_pass = "PcaVXEZo52c7ktes"
+    sender = 'no-reply@mail.x-zhou.com'
+    receivers = [receiver_email]
+    email_message = MIMEText(email_content, 'plain', 'utf-8')
+    email_message['From'] = Header("mail.x-zhou.com", 'utf-8')
+    email_message['To'] = Header(receiver_email, 'utf-8')
+
+    subject = email_subject
+    email_message['Subject'] = Header(subject, 'utf-8')
+
+    server = smtplib.SMTP_SSL(smtp_host,465)
+    server.login(smtp_user,smtp_pass)
+    server.sendmail(sender, receivers, email_message.as_string())
+
+#send_email('852940804@qq.com','test email','TEST')
+
