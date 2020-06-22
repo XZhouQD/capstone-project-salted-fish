efe7c00c68d5418ea33cbdc538b622d3d5e69cb9
Xiaowei-Zhou <xiaowei.zhou@unsw.edu.au>
2020-06-22 16:10:16 +0800

    fix pre_init, admin, dreamer db login

--- backend/pre_init.py
+++ backend/pre_init.py
@@ -12,32 +12,39 @@ from db import create_conn
 print("Start database pre initialisation")
 conn = create_conn()
 print("default database connection established")
-# admin table
-print("rebuild admin table...")
+
+print("clean all existing tables")
+
+query = 'drop table if exists skills;'
+conn.execute(query)
+
 query = 'drop table if exists admin;'
 conn.execute(query)
-query = 'CREATE TABLE admin (ID INT(11) NOT NULL AUTO_INCREMENT, name VARCHAR(50) DEFAULT NULL, email VARCHAR(256) DEFAULT NULL, password VARCHAR(64), create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY (`id`));'
+
+query = 'drop table if exists collaborator CASCADE;'
+conn.execute(query)
+
+query = 'drop table if exists dreamer;'
+conn.execute(query)
+
+# admin table
+print("rebuild admin table...")
+query = 'CREATE TABLE admin (ID INT(11) NOT NULL AUTO_INCREMENT, name VARCHAR(50) DEFAULT NULL, email VARCHAR(256) DEFAULT NULL, password VARCHAR(64), create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY (`id`), UNIQUE KEY (`email`));'
 conn.execute(query)
 
 # collaborator table
 print("rebuild collaborator table...")
-query = 'drop table if exists collaborator;'
-conn.execute(query)
 # education: -1 = default, 1 = Other, 2 = bachelor, 3 = master, 4 = PhD
-query = 'CREATE TABLE collaborator (ID INT(11) NOT NULL AUTO_INCREMENT, name VARCHAR(50) DEFAULT NULL, email VARCHAR(256) DEFAULT NULL, password VARCHAR(64), phone_no VARCHAR(20) DEFAULT NULL, education TINYINT DEFAULT -1, user_level INT(5), description TEXT, create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY (`ID`));'
+query = 'CREATE TABLE collaborator (ID INT(11) NOT NULL AUTO_INCREMENT, name VARCHAR(50) DEFAULT NULL, email VARCHAR(256) DEFAULT NULL, password VARCHAR(64), phone_no VARCHAR(20) DEFAULT NULL, education TINYINT DEFAULT -1, user_level INT(5), description TEXT, create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY (`ID`), UNIQUE KEY (`email`));'
 conn.execute(query)
 
 # dreamer table
 print("rebuild dreamer table...")
-query = 'drop table if exists dreamer;'
-conn.execute(query)
-query = 'CREATE TABLE dreamer (ID INT(11) NOT NULL AUTO_INCREMENT, name VARCHAR(50) DEFAULT NULL, email VARCHAR(256) DEFAULT NULL, password VARCHAR(64), phone_no VARCHAR(20) DEFAULT NULL, user_level INT(5), description TEXT, create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY (`ID`));'
+query = 'CREATE TABLE dreamer (ID INT(11) NOT NULL AUTO_INCREMENT, name VARCHAR(50) DEFAULT NULL, email VARCHAR(256) DEFAULT NULL, password VARCHAR(64), phone_no VARCHAR(20) DEFAULT NULL, user_level INT(5), description TEXT, create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY (`ID`), UNIQUE KEY (`email`));'
 conn.execute(query)
 
 # skill/exp table
 print("rebuild skill/exp table...")
-query = 'drop table if exists skills;'
-conn.execute(query)
 query = 'CREATE TABLE skills (skill TINYINT DEFAULT -1, experience TINYINT DEFAULT 0, collaboratorID int(11) NOT NULL, FOREIGN KEY (`collaboratorID`) REFERENCES collaborator(`ID`));'
 conn.execute(query)
 
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,7 +1,37 @@
 #!/usr/bin/python3
 
-from utils import sha256
+from users.util import sha256
 
 class Admin():
+    def __init__(self, name, email, password_plain='', password_encrypted='', id='', create_time='', last_update=''):
+        self.name = name
+        self.email = email
+        if password_encrypted == '':
+            self.password_encrypted = sha256(password_plain)
+        else:
+            self.password_encrypted = password_encrypted
+        self.id = id
+        self.create_time = create_time
+        self.last_update = last_update
 
+    @staticmethod
+    def login(conn, email, password_plain='', password_encrypted=''):
+        query = "select * from admin where email = \'" + email + "\';"
+        result = conn.execute(query)
+        if result.rowcount == 0:
+            return None
+        row = result.fetchone()
+        if password_encrypted == '':
+            enc_pass = sha256(password_plain)
+        else:
+            enc_pass = password_encrypted
+        if enc_pass == row['password']:
+            return Admin(row['name'], row['email'], password_encrypted=row['password'], id=row['ID'], create_time=row['create_time'], last_update=row['last_update']).info()
+        return None
 
+    def info(self):
+        return {'role': 'Admin', 'name': self.name, 'email': self.email, 'id': self.id, 'creation_time': self.create_time, 'last_update': self.last_update}
+
+    def commit(self, conn):
+        query = "INSERT INTO admin (name, email, password) VALUES (\'" + self.name + "\', \'" + self.email + "\', \'" + self.password_encrypted + "\') ON DUPLICATE KEY UPDATE `name`= \'" + self.name + "\', `password` = \'" + self.password_encrypted + "\';"
+        conn.execute(query)
