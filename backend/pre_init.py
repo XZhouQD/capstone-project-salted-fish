b2981b10d27c0d4f5701c54b9bfd7efe0f475d6e
Xiaowei-Zhou <xiaowei.zhou@unsw.edu.au>
2020-06-24 11:24:11 +0800

    fix pre_init CASCADE

--- backend/pre_init.py
+++ backend/pre_init.py
@@ -21,16 +21,10 @@ conn.execute(query)
 query = 'drop table if exists admin;'
 conn.execute(query)
 
-query = 'drop table if exists collaborator CASCADE;'
+query = 'drop table if exists collaborator;'
 conn.execute(query)
 
 query = 'drop table if exists dreamer;'
-conn.execute(query)
-
-query = 'drop table if exists project;'
-conn.execute(query)
-
-query = 'drop table if exists project_role;'
 conn.execute(query)
 
 query = 'drop table if exists application;'
@@ -38,6 +32,13 @@ conn.execute(query)
 
 query = 'drop table if exists invitation;'
 conn.execute(query)
+
+query = 'drop table if exists project_role;'
+conn.execute(query)
+
+query = 'drop table if exists project;'
+conn.execute(query)
+
 
 # admin table
 print("rebuild admin table...")
@@ -63,63 +64,63 @@ conn.execute(query)
 # project table
 print("rebuild project table...")
 query = '''CREATE TABLE project (
-ID INT(11) NOT NULL AUTO_INCREMENT, 
-project_title VARCHAR(50) DEFAULT NULL, 
-description VARCHAR(256) DEFAULT NULL,
+ID INT(11) NOT NULL AUTO_INCREMENT,
+project_title VARCHAR(100) DEFAULT NULL,
+description TEXT DEFAULT NULL,
 category VARCHAR(256) DEFAULT NULL,
 dreamerID int(11) NOT NULL,FOREIGN KEY (`dreamerID`) REFERENCES dreamer(`ID`),
 project_status TINYINT DEFAULT -1,
-is_hidden TINYINT DEFAULT -1,
+is_hidden TINYINT DEFAULT 0,
 hidden_reason VARCHAR(256) DEFAULT NULL,
-create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
-last_update_data TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+is_modified_after_hidden TINYINT DEFAULT 0,
+create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 PRIMARY KEY (`ID`));'''
 conn.execute(query)
 
 # project_role table
 print("rebuild project_role table...")
 query = '''CREATE TABLE project_role (
-role_ID INT(11) NOT NULL AUTO_INCREMENT,
-projectID int(11) NOT NULL,FOREIGN KEY (`projectID`) REFERENCES project(`ID`),
+ID INT(11) NOT NULL AUTO_INCREMENT,
+projectID int(11) NOT NULL, FOREIGN KEY (`projectID`) REFERENCES project(`ID`),
 title VARCHAR(50) DEFAULT NULL,
-amount INT(50) DEFAULT NULL,
+amount INT(3) DEFAULT NULL,
 skill TINYINT DEFAULT -1,
 experience TINYINT DEFAULT 0,
 education TINYINT DEFAULT -1,
-general_enquiry VARCHAR(256) DEFAULT NULL,
+general_enquiry VARCHAR(2048) DEFAULT NULL,
 create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-last_update_data TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-UNIQUE KEY (`role_ID`));'''
+last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY (`ID`));'''
 conn.execute(query)
 
 # application table
 print("rebuild application table...")
 query = '''CREATE TABLE application(
-appli_id INT(11) NOT NULL AUTO_INCREMENT,
+ID INT(11) NOT NULL AUTO_INCREMENT,
 projectID int(11) NOT NULL,FOREIGN KEY (`projectID`) REFERENCES project(`ID`),
-role_applied int(11) NOT NULL,FOREIGN KEY (`role_applied`) REFERENCES project_role(`role_ID`),
+role_applied int(11) NOT NULL,FOREIGN KEY (`role_applied`) REFERENCES project_role(`ID`),
 applicant int(11) NOT NULL,FOREIGN KEY (`applicant`) REFERENCES collaborator(`ID`),
 status TINYINT DEFAULT -1,
-general_text VARCHAR(256) DEFAULT NULL,
+general_text TEXT DEFAULT NULL,
 create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-last_update_data TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-UNIQUE KEY (`appli_id`));'''
+last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY (`ID`));'''
 conn.execute(query)
 
 # invitation table
 print("rebuild invitation table...")
 query = '''CREATE TABLE invitation(
-invi_id INT(11) NOT NULL AUTO_INCREMENT,
+ID INT(11) NOT NULL AUTO_INCREMENT,
 projectID int(11) NOT NULL,FOREIGN KEY (`projectID`) REFERENCES project(`ID`),
-role_applied int(11) NOT NULL,FOREIGN KEY (`role_applied`) REFERENCES project_role(`role_ID`),
-applicant int(11) NOT NULL,FOREIGN KEY (`applicant`) REFERENCES collaborator(`ID`),
+role_invited int(11) NOT NULL,FOREIGN KEY (`role_invited`) REFERENCES project_role(`ID`),
 invitor int(11) NOT NULL,FOREIGN KEY (`invitor`) REFERENCES dreamer(`ID`),
 invitee int(11) NOT NULL,FOREIGN KEY (`invitee`) REFERENCES collaborator(`ID`),
 status TINYINT DEFAULT -1,
-general_text VARCHAR(256) DEFAULT NULL,
+general_text TEXT DEFAULT NULL,
 create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-last_update_data TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-UNIQUE KEY (`invi_id`));'''
+last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY (`ID`));'''
 conn.execute(query)
 
 print("pre initialisation complete")
