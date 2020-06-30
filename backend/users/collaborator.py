7d188c562acb663f1f1ea954de97dbaaf71a24c2
unswbei <66148112+unswbei@users.noreply.github.com>
2020-06-30 15:27:45 +1000

    Update collaborator.py

--- backend/users/collaborator.py
+++ backend/users/collaborator.py
@@ -90,10 +90,14 @@ class Collaborator():
         return False
 
     @staticmethod
-    def commit_newpassword(conn, email,new_password):
-        email = email.lower()
-        query = "UPDATE collaborator set password_plain = new_password ,password_encrypted = sha256(new_password) where email = \'" + email + "\';"
-        conn.execute(query)
+    def commit_newpassword(conn, email, password_plain='', password_encrypted=''):
+        email = email.lower()
+        if password_encrypted == '':
+            new_pass = sha256(password_plain)
+        else:
+            new_pass = password_encrypted
+        query = "UPDATE collaborator set password = \'" + new_pass + "\' where email = \'" + email + "\';"
+        conn.execute(query)
 
     def info(self):
         return {'role': 'Collaborator', 'name': self.name, 'email': self.email, 'id': self.id, 'creation_time': self.create_time, 'last_update': self.last_update, 'phone_no': self.phone_no, 'user_level': self.level_text, 'description': self.description, 'education': self.education_text, 'skills': self.skill_dict}
