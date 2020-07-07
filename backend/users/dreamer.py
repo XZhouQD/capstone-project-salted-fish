aa9bf25f6370bdd524c635f1acdc36e3600fd1ab
Lisa-King <jing11.lisa@gmail.com>
2020-07-07 22:34:21 +1000

    recommendation for dreamer

--- backend/users/dreamer.py
+++ backend/users/dreamer.py
@@ -56,7 +56,57 @@ class Dreamer():
         row = result.fetchone()
         return Dreamer(row['name'], row['email'], password_encrypted=row['password'], id=row['ID'], create_time=row['create_time'], last_update=row['last_update'], phone_no=row['phone_no'], user_level=row['user_level'], description=row['description'])
 
+    @staticmethod
     def collaborators_recommdation(self, conn):
+        owner = self.id
+        #fetch all project owned by dreamer;
+        query_1 = "SELECT ID as proj_ID, project_title, description, category, dreamerID, project_status, is_hidden, hidden_reason, is_modified_after_hidden FROM project WHERE dreamerID = " + str(owner) + " and project_status = 1 ORDER BY ID;"
+        result_1 = conn.execute(query_1)
+        if result_1.rowcount == 0:
+            return None
+        project_role_recomm_collabors_list = []
+        for i in range(result_1.rowcount):
+            row_1 = result_1.fetchone()
+            #fetch roles for each project;
+            query_2 = "SELECT ID as role_ID, title, amount, skill, experience, education, general_enquiry FROM project_role WHERE projectID " + str(row_1['proj_ID']) + " ORDER BY ID;"
+            result_2 = conn.execute(query_2)
+            if result_2.rowcount == 0:
+                continue
+            #macthing collabors based on the requirements of each role;
+            else:
+                role_recomm_collabors_list = []
+                for j in range(result_2.rowcount):
+                    row_2 = result_2.fetchone()
+                    collaborator_list = [] 
+                    strict_matching_count = 0
+                    relaxing_matching_count = 0
+                    #1.strict matching
+                    query_3 = "select collaborator.ID as ID, name, email, phone_no, education, skill, experience, user_level, description from collaborator, skills where collaborator.ID = skills.collaboratorID and skill = " + str(row_2['skill']) + " and experience = " + str(row_2['experience']) + " and education = " + str(row_2['education']) + " ORDER BY ID;"
+                    result_3 = conn.execute(query_3)
+                    if result_3.rowcount != 0:
+                        for k in range(result_3.rowcount):
+                            row_3 = result_3.fetchone()
+                            collabor = {'CollaboratorID':row_3['ID'],'Name':row_3['name'],'Email':row_3['email'],'Phone_no':row_3['phone_no'],'Skill':row_3['skill'],'Experience':row_3['experience'],'Education':row_3['education'],'User_level':row_3['user_level'],'Description':row_3['description']}
+                            collaborator_list.append(collabor)
+                            strict_matching_count += 1
+                    #2.relaxing matching
+                    query_4 = "select collaborator.ID as ID, name, email, phone_no, education, skill, experience, user_level, description from collaborator, skills where collaborator.ID = skills.collaboratorID and skill = " + str(row_2['skill']) + " and experience >= " + str(row_2['experience'] - 1) + " and education >= " + str(row_2['education'] - 1) + " ORDER BY ID;"
+                    result_4 = conn.execute(query_4)
+                    if result_4.rowcount != 0:
+                        for l in range(result_4.rowcount):
+                            row_4 = result_4.fetchone()
+                            collabor = {'CollaboratorID':row_4['ID'],'Name':row_4['name'],'Email':row_4['email'],'Phone_no':row_5['phone_no'],'Skill':row_4['skill'],'Experience':row_4['experience'],'Education':row_4['education'],'User_level':row_4['user_level'],'Description':row_4['description']}
+                            if collabor not in collaborator_list:
+                                collaborator_list.append(collabor)
+                            relaxing_matching_count += 1
+                    #role info + recommendation collabor list
+                    role_recomm_collabors = {'Role_ID':row_2['role_ID'], 'Title':row_2['title'], 'Amount':row_2['amount'], 'Skill':row_2['skill'], 'Experience':row_2['experience'], 'Education':row_2['education'], 'General_enquiry':row_2['general_enquiry'],'Strict_matching_count':strict_matching_count, 'Relaxing_matching_count':relaxing_matching_count, 'Collaborator_list':collaborator_list}
+                    role_recomm_collabors_list.append(role_recomm_collabors)
+            #project info + role info + recommendation collabor list
+            project_role_recomm_collabors = {'Project_ID':row_1['proj_ID'], 'Project_title':row_1['project_title'], 'Description':row_1['description'], 'Category':row_1['category'], 'DreamerID':row_1['dreamerID'], 'Project_status':row_1['project_status'], 'Is_hidden':row_1['is_hidden'], 'Hidden_reason':row_1['hidden_reason'], 'Is_modified_after_hidden':row_1['is_modified_after_hidden'], 'Role_recomm_collabors_list':role_recomm_collabors_list}
+            project_role_recomm_collabors_list.append(project_role_recomm_collabors)
+
+    def collaborators_recommdation_1(self, conn):
         owner = self.id
         roles_needed = {}
         proj_role_collaborator_list = []
@@ -76,7 +126,7 @@ class Dreamer():
             if result.rowcount != 0:
                 for j in range(result.rowcount):
                     row = result.fetchone()
-                    collabor = {'collaboratorID':row['ID'],'Name':row['name'],'Email':row['email'],'Phone_no':row['phone_no'],'skill':row['skill'],'experience':row['experience'],'education':row['education'],'user_level':row['user_level'],'description':row['description']}
+                    collabor = {'CollaboratorID':row['ID'],'Name':row['name'],'Email':row['email'],'Phone_no':row['phone_no'],'Skill':row['skill'],'Experience':row['experience'],'Education':row['education'],'User_level':row['user_level'],'Description':row['description']}
                     proj_role_info = Project.get_by_id_skill(conn, roles_needed[role_i][0], roles_needed[role_i][1])
                     proj_role_collaborator_list.append((proj_role_info, collabor))
                     strict_matching_count += 1
@@ -89,7 +139,7 @@ class Dreamer():
             if result.rowcount != 0:
                 for j in range(result.rowcount):
                     row = result.fetchone()
-                    collabor = {'collaboratorID':row['ID'],'Name':row['name'],'Email':row['email'],'Phone_no':row['phone_no'],'skill':row['skill'],'experience':row['experience'],'education':row['education'],'user_level':row['user_level'],'description':row['description']}
+                    collabor = {'CollaboratorID':row['ID'],'Name':row['name'],'Email':row['email'],'Phone_no':row['phone_no'],'Skill':row['skill'],'Experience':row['experience'],'Education':row['education'],'User_level':row['user_level'],'Description':row['description']}
                     proj_role_info = Project.get_by_id_skill(conn, roles_needed[role_i][0], roles_needed[role_i][1])
                     proj_role_collaborator_list.append((proj_role_info, collabor))
                     relaxing_matching_count += 1
