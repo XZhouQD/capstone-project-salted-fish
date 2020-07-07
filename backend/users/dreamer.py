410923eab3eea2aa9c87d38f72a4ef224c0bb448
Lisa-King <jing11.lisa@gmail.com>
2020-07-07 22:44:30 +1000

    recommendation for dreamer

--- backend/users/dreamer.py
+++ backend/users/dreamer.py
@@ -57,6 +57,15 @@ class Dreamer():
         return Dreamer(row['name'], row['email'], password_encrypted=row['password'], id=row['ID'], create_time=row['create_time'], last_update=row['last_update'], phone_no=row['phone_no'], user_level=row['user_level'], description=row['description'])
 
     @staticmethod
+    def get_by_id(conn, id):
+        query = "select * from dreamer where ID = " + int(id) + ";"
+        result = conn.execute(query)
+        if result.rowcount == 0:
+            return None
+        row = result.fetchone()
+        return Dreamer(row['name'], row['email'], password_encrypted=row['password'], id=row['ID'], create_time=row['create_time'], last_update=row['last_update'], phone_no=row['phone_no'], user_level=row['user_level'], description=row['description'])
+
+    @staticmethod
     def collaborators_recommdation(self, conn):
         owner = self.id
         #fetch all project owned by dreamer;
