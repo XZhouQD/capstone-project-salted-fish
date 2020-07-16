5ec415680ca1060f66a61b9c6d93b9b4ea5d9c8c
Xiaowei-Zhou <xiaowei.zhou@unsw.edu.au>
2020-07-16 19:52:44 +0800

    new database structure

--- backend/pre_init.py
+++ backend/pre_init.py
@@ -7,7 +7,7 @@ that all existing data will be deleted.
 Use with CAUTION!
 '''
 
-from db import create_conn
+from db import DB
 import yaml
 from users.admin import Admin
 from users.collaborator import Collaborator
@@ -19,7 +19,8 @@ f = open('server.config', 'r', encoding='utf-8')
 config = yaml.load(f.read(), Loader=yaml.FullLoader)
 
 print("Start database pre initialisation")
-conn = create_conn()
+db = DB()
+conn = db.conn()
 print("default database connection established")
 
 print("======= clean all existing tables =======")
@@ -52,6 +53,10 @@ print("invitation")
 query = 'drop table if exists invitation;'
 conn.execute(query)
 
+print("role_skill")
+query = 'drop table if exists role_skill;'
+conn.execute(query)
+
 print("project_role")
 query = 'drop table if exists project_role;'
 conn.execute(query)
@@ -76,23 +81,57 @@ conn.execute(query)
 print("======= rebuild tables =======")
 # admin table
 print("rebuild admin table...")
-query = 'CREATE TABLE admin (ID INT(11) NOT NULL AUTO_INCREMENT, name VARCHAR(50) DEFAULT NULL, email VARCHAR(256) DEFAULT NULL, password VARCHAR(64), create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY (`id`), UNIQUE KEY (`email`));'
+query = '''CREATE TABLE admin (
+ID INT(11) NOT NULL AUTO_INCREMENT,
+name VARCHAR(50) DEFAULT NULL,
+email VARCHAR(256) DEFAULT NULL,
+password VARCHAR(64),
+create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY (`id`),
+UNIQUE KEY (`email`));'''
 conn.execute(query)
 
 # collaborator table
 print("rebuild collaborator table...")
 # education: -1 = default, 1 = Other, 2 = bachelor, 3 = master, 4 = PhD
-query = 'CREATE TABLE collaborator (ID INT(11) NOT NULL AUTO_INCREMENT, name VARCHAR(50) DEFAULT NULL, email VARCHAR(256) DEFAULT NULL, password VARCHAR(64), phone_no VARCHAR(20) DEFAULT NULL, education TINYINT DEFAULT -1, user_level INT(5), description TEXT, create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY (`ID`), UNIQUE KEY (`email`));'
+query = '''CREATE TABLE collaborator (
+ID INT(11) NOT NULL AUTO_INCREMENT,
+name VARCHAR(50) DEFAULT NULL,
+email VARCHAR(256) DEFAULT NULL,
+password VARCHAR(64),
+phone_no VARCHAR(20) DEFAULT NULL,
+education TINYINT DEFAULT -1,
+user_level INT(5), description TEXT,
+create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY (`ID`),
+UNIQUE KEY (`email`));'''
 conn.execute(query)
 
 # dreamer table
 print("rebuild dreamer table...")
-query = 'CREATE TABLE dreamer (ID INT(11) NOT NULL AUTO_INCREMENT, name VARCHAR(50) DEFAULT NULL, email VARCHAR(256) DEFAULT NULL, password VARCHAR(64), phone_no VARCHAR(20) DEFAULT NULL, user_level INT(5), description TEXT, create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY (`ID`), UNIQUE KEY (`email`));'
+query = '''CREATE TABLE dreamer (
+ID INT(11) NOT NULL AUTO_INCREMENT,
+name VARCHAR(50) DEFAULT NULL,
+email VARCHAR(256) DEFAULT NULL,
+password VARCHAR(64),
+phone_no VARCHAR(20) DEFAULT NULL,
+user_level INT(5),
+description TEXT,
+create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+PRIMARY KEY (`ID`),
+UNIQUE KEY (`email`));'''
 conn.execute(query)
 
 # skill/exp table
 print("rebuild skill/exp table...")
-query = 'CREATE TABLE skills (skill TINYINT DEFAULT -1, experience TINYINT DEFAULT 0, collaboratorID int(11) NOT NULL, FOREIGN KEY (`collaboratorID`) REFERENCES collaborator(`ID`));'
+query = '''CREATE TABLE skills (
+skill TINYINT DEFAULT -1,
+experience TINYINT DEFAULT 0,
+collaboratorID int(11) NOT NULL,
+FOREIGN KEY (`collaboratorID`) REFERENCES collaborator(`ID`));'''
 conn.execute(query)
 
 # Always create No.1 collaborator/dreamer/admin for system keeping to avoid foreign key error
@@ -132,6 +171,15 @@ general_enquiry VARCHAR(2048) DEFAULT NULL,
 create_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 PRIMARY KEY (`ID`));'''
+conn.execute(query)
+
+# role_skill table
+print("rebuild role_skill table...")
+query = '''CREATE TABLE role_skill (
+skill TINYINT DEFAULT -1,
+experience TINYINT DEFAULT 0,
+roleID int(11) NOT NULL,
+FOREIGN KEY (`roleID`) REFERENCES project_role(`ID`));'''
 conn.execute(query)
 
 # application table
